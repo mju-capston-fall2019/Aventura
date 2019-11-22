302d5c036c1bf73195481b5c4fc7d04d614250d6
Kangsan Chang <kangsanchang@gmail.com>
2019-11-23 05:19:24 +0900

    ADD RecommendationScreen
    - Rename main_page to RecommendationScreen
    - ADD Model for Recommendation Cards

---
+++ lib/models/RecommendationCardModel.dart
@@ -0,0 +1,10 @@
+class RecommendationCardModel {
+
+  String id;
+  String title;
+  String type;
+  double distance;
+
+  RecommendationCardModel(this.id, this.title, this.type, this.distance);
+
+}
---
+++ lib/screens/RecommendationScreen.dart
@@ -0,0 +1,205 @@
+import 'package:aventura/models/RecommendationCardModel.dart';
+import 'package:flutter/material.dart';
+
+class RecommendationScreen extends StatefulWidget {
+  @override
+  _RecommendationScreenState createState() => _RecommendationScreenState();
+}
+
+class _RecommendationScreenState extends State<RecommendationScreen>
+    with TickerProviderStateMixin {
+  var cardsList = [
+    RecommendationCardModel('1', "Colosseo", "Historic site", 0.7),
+    RecommendationCardModel('2', "Foro Romano", "Religion", 1.5),
+    RecommendationCardModel('3', "Bocca della Verità", "Art gallery", 6.5)
+  ];
+  ScrollController scrollController;
+  AnimationController animationController;
+  CurvedAnimation curvedAnimation;
+  var cardIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    scrollController = new ScrollController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [
+              Color.fromRGBO(186, 104, 200, 1),
+              Color.fromRGBO(74, 22, 140, 1)
+            ])),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Container(
+                width: size.width * 0.9,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 32.0, vertical: 20.0),
+                  child: Container(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Padding(
+                          padding:
+                              const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
+                          child: Text(
+                            "Friday",
+                            style: TextStyle(
+                                fontSize: 30.0,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w400),
+                          ),
+                        ),
+                        Padding(
+                          padding:
+                              const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 12.0),
+                          child: Text(
+                            "22 Nov",
+                            style: TextStyle(
+                                fontSize: 25.0,
+                                color: Color.fromRGBO(255, 255, 255, 0.6),
+                                fontWeight: FontWeight.w400),
+                          ),
+                        ),
+                        Text(
+                          "kangsan, You have 3 places to go!",
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            color: Color.fromRGBO(255, 255, 255, 0.6),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(bottom: 30.0),
+                height: 400.0,
+                child: ListView.builder(
+                    physics: NeverScrollableScrollPhysics(),
+                    itemCount: 3,
+                    controller: scrollController,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, position) {
+                      return GestureDetector(
+                        onHorizontalDragEnd: (details) {
+                          animationController = AnimationController(
+                              vsync: this,
+                              duration: Duration(milliseconds: 500));
+                          curvedAnimation = CurvedAnimation(
+                              parent: animationController,
+                              curve: Curves.fastOutSlowIn);
+                          if (details.velocity.pixelsPerSecond.dx > 0) {
+                            if (cardIndex > 0) {
+                              cardIndex--;
+                            }
+                          } else {
+                            if (cardIndex < cardsList.length - 1) {
+                              cardIndex++;
+                            }
+                          }
+                          setState(() {
+                            scrollController.animateTo((cardIndex) * 256.0,
+                                duration: Duration(milliseconds: 500),
+                                curve: Curves.fastOutSlowIn);
+                          });
+                          animationController.forward();
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Card(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0)),
+                            child: Container(
+                              width: 250.0,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  Padding(
+                                    padding: const EdgeInsets.only(
+                                        right: 8.0, left: 8.0, top: 4.0),
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: <Widget>[
+                                        Chip(
+                                          label: new Text(
+                                            cardsList[position].type,
+                                            style:
+                                                TextStyle(color: Colors.white),
+                                          ),
+                                          backgroundColor: Colors.black38,
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 8.0, vertical: 4.0),
+                                    child: Container(
+                                      height: 200,
+                                      child: FittedBox(
+                                        child: Image.asset(
+                                            "assets/colosseum/image1.jpg"),
+                                        fit: BoxFit.fitHeight,
+                                      ),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: <Widget>[
+                                        Padding(
+                                          padding: const EdgeInsets.symmetric(
+                                              horizontal: 8.0, vertical: 4.0),
+                                          child: Text(
+                                            "${cardsList[position].distance} Km from here",
+                                            style: TextStyle(
+                                                color: Colors.grey,
+                                                fontSize: 18.0),
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding: const EdgeInsets.symmetric(
+                                              horizontal: 8.0, vertical: 4.0),
+                                          child: Text(
+                                            "${cardsList[position].title}",
+                                            style: TextStyle(fontSize: 28.0),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    }),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
