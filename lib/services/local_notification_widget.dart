f70fd83815456eb4f12a579fccb3a382f2c0e2e7
Kangsan Chang <kangsanchang@gmail.com>
2019-11-28 22:19:10 +0900

    Fix keyboard hiding bug in TextFormField
    - Change _formkey & controller to static variable
    - Add logout button in test page

--- lib/services/local_notification_widget.dart
+++ lib/services/local_notification_widget.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:aventura/services/local_notications_helper.dart';
@@ -42,29 +43,10 @@ class _LocalNotificationWidgetState extends State<LocalNotificationWidget> {
               onPressed: () => showOngoingNotification(notifications,
                   title: 'Aventura', body: '콜로세움에 들어오셨습니다.'),
             ),
-//           RaisedButton(
-//              child: Text('Replace notification'),
-//              onPressed: () => showOngoingNotification(notifications,
-//                  title: 'ReplacedTitle', body: 'ReplacedBody'),
-//            ),
-//            RaisedButton(
-//              child: Text('Other notification'),
-//              onPressed: () => showOngoingNotification(notifications,
-//                  title: 'OtherTitle', body: 'OtherBody', id: 20),
-//            ),
-//            const SizedBox(height: 32),
-//            title('Feautures'),
-//            RaisedButton(
-//              child: Text('Silent notification'),
-//              onPressed: () => showSilentNotification(notifications,
-//                  title: 'SilentTitle', body: 'SilentBody', id: 30),
-//            ),
-//            const SizedBox(height: 32),
-//            title('Cancel'),
-//            RaisedButton(
-//              child: Text('Cancel all notification'),
-//              onPressed: notifications.cancelAll,
-//            ),
+            RaisedButton(
+              child: Text('logout'),
+              onPressed: () => FirebaseAuth.instance.signOut(),
+            )
           ],
         ),
       );
